8f785bfa7160173dfa95e5418d8d34b62a0301bd
zylai <40312846+zylai@users.noreply.github.com>
2020-11-04 19:10:30 -0800

    Update ttt.sh

--- ttt.sh
+++ ttt.sh
@@ -78,44 +78,6 @@ num2grid()
 	esac
 }
 
-prompt()
-{
-	draw
-	printf "Choose your move: "
-	read choice
-	if [[ "$choice" =~ [a-cA-C][1-3] ]]
-		then
-			insert $choice
-			return 0
-		else
-			echo "Invalid input. Try again. Example: a1, b3, C2, etc."
-			echo "Choose your move: "
-			return 1
-		fi
-}
-
-insert()
-{
-	move=`echo $1`
-	num=`grid2num $move`
-	num_m1=$(($num-1))
-	current=`cat board.txt | cut -c$num`
-
-	if [[ "$current" == "-" ]]
-	then
-		#insert the move into database https://stackoverflow.com/a/24470008
-		updated_board=`sed -E "s/^(.{$num_m1})-/\1X/" board.txt`
-		rm board.txt
-		echo $updated_board > board.txt
-		#declare $move="x"
-		return 0
-	else
-		echo "Invalid move. Square already occupied. Try again."
-		echo "Choose your move: "
-		return 1
-	fi
-}
-
 # 10 player wins
 # 20 opponent (computer) wins
 # 30 no winner, moves still available
@@ -151,44 +113,112 @@ check()
 	fi
 }
 
-echo "---------" > board.txt
+replay=y
 
-while [ true ]
+while [[ "$replay" == y ]]
 do
-	prompt
-	lastmove=`echo $?`
-	check
-	result=`echo $?`
+	echo "---------" > board.txt
+	result=30
+	draw
 
-	if [[ "$result" == 10 ]]
-	then
-		echo "You win!"
-		printf "Play again? y|n: "
-		read replay
-		if [[ "$replay" == "y" ]]
+	while [[ "$result" == "30" ]]
+	do
+		while [ true ]
+		do
+			printf "Choose your move: "
+			read move
+
+			num=`grid2num $move` 2>/dev/null
+			num_m1=$(($num-1)) 2>/dev/null
+			current=`cat board.txt 2>/dev/null | cut -c$num 2>/dev/null` 2>/dev/null
+
+			if [[ "$move" =~ [a-cA-C][1-3] && "$current" == "-" ]]
+			then
+				#insert the move into database https://stackoverflow.com/a/24470008
+				updated_board=`sed -E "s/^(.{$num_m1})-/\1X/" board.txt`
+				rm board.txt
+				echo $updated_board > board.txt
+				break
+			else
+				echo "Invalid input or move. Try again."
+			fi
+		done
+
+		check
+		result=`echo $?`
+
+		if [[ "$result" == "10" || "$result" == "40" ]]
 		then
-			echo "---------" > board.txt
-		else
-			echo "Bye!"
-			exit 0
+			case "$result" in
+				10)
+					echo "You win"
+					printf "Play again? y|n: "
+					read replay
+					if [[ "$replay" == "y" || "$replay" == "Y" ]]
+					then
+						break
+					else
+						echo "Bye!"
+						exit 0
+					fi
+					;;
+				40)
+					echo "Tie"
+					printf "Play again? y|n: "
+					read replay
+					if [[ "$replay" == "y" || "$replay" == "Y" ]]
+					then
+						break
+					else
+						echo "Bye!"
+						exit 0
+					fi
+					;;
+			esac
 		fi
-	fi
+
+		#computer's turn
+		square=`cat board.txt | fold -w1 | grep -n "-" | sort -R | head -n1 | cut -c1`
+		square_m1=$(($square-1))
+		updated_board=`sed -E "s/^(.{$square_m1})-/\1O/" board.txt`
+		rm board.txt
+		echo $updated_board > board.txt
+
+		check
+		result=`echo $?`
+
+		if [[ "$result" == "20" || "$result" == "40" ]]
+		then
+			case "$result" in
+				20)
+					echo "Opponent's move: `num2grid $square`"
+					echo "You lost"
+					printf "Play again? y|n: "
+					read replay
+					if [[ "$replay" == "y" || "$replay" == "Y" ]]
+					then
+						break
+					else
+						echo "Bye!"
+						exit 0
+					fi
+					;;
+				40)
+					echo "Opponent's move: `num2grid $square`"
+					echo "Tie"
+					printf "Play again? y|n: "
+					read replay
+					if [[ "$replay" == "y" || "$replay" == "Y" ]]
+					then
+						break
+					else
+						echo "Bye!"
+						exit 0
+					fi
+					;;
+			esac
+		fi
+		draw
+		echo "Opponent's move: `num2grid $square`"
+	done
 done
-
-
-
-#if [ $result -eq 10 ]
-#then
-	# player wins
-#else
-	# computer makes a move
-#	check
-#	if [ $result -eq 20 ]
-#	then
-		# computer wins
-#	fi
-#fi
-
-
-
-
